54bc08ecf5481ddaa467620672002b86dc7302c0
eliezerqueiroz <eliezerqueiroz.lima@gmail.com>
2020-11-15 04:18:26 -0300

    layout update

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("hidden-logo").style.visibility = "hidden";
+  }
+  
+  function closeMenu() {
+    document.getElementById("mySidenav").style.width = "0";
+    
+    document.getElementById("hidden-logo").style.visibility = "visible";
+  }
+  
