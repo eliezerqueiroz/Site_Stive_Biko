3c5f227427e3335d22bc5bc62f4a2c3ecf17339a
eliezerqueiroz <eliezerqueiroz.lima@gmail.com>
2020-11-19 18:48:50 -0300

    responsive layout final

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 function openMenu() {
     document.getElementById("mySidenav").style.width = "300px";
+    // document.getElementById("mySidenav").style.visibility = "visible";
     document.getElementById("hidden-logo").style.visibility = "hidden";
   }
   
   function closeMenu() {
     document.getElementById("mySidenav").style.width = "0";
-    
+    // document.getElementById("mySidenav").style.visibility = "hidden";
     document.getElementById("hidden-logo").style.visibility = "visible";
   }
   
+  // var largura = document.body.style.width
+
+  // if(largura >= 100){
+  //   document.getElementById("mySidenav").style.width = "300px";
+  // }
